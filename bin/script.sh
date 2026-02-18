9b669b8a5621a7c292bba74e2b08cbd9b37ef950
Sam Fuchs <samcfuchs@gmail.com>
2026-02-18 11:56:47 -0800

    Fix linking script

--- bin/script.sh
+++ bin/script.sh
@@ -3,5 +3,5 @@
 #
 
 ln -svf --backup ~/dot/i3/config ~/.config/i3/config
-ln -svf --backup ~/dot/polybar/{config.ini, launch.sh} ~/.config/polybar/
-ln -svf --backup ./zsh/.zshrc $HOME/.zshrc
+ln -svf --backup ~/dot/polybar/{config.ini,launch.sh} ~/.config/polybar/
+ln -svf --backup ~/zsh/.zshrc ~/.zshrc
