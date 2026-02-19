257ac56e00d348b7afb8601ce49a11f8a618d19e
Sam Fuchs <samcfuchs@gmail.com>
2026-02-18 16:31:04 -0800

    Fix hourly chime

--- bin/script.sh
+++
@@ -1,7 +0,0 @@
-#ln -svf $HOME/dot/* $XDG_CONFIG_HOME
-#ln -svf $HOME/dot/zsh/.zshenv $HOME/.zshenv
-#
-
-ln -svf --backup ~/dot/i3/config ~/.config/i3/config
-ln -svf --backup ~/dot/polybar/{config.ini,launch.sh} ~/.config/polybar/
-ln -svf --backup ~/zsh/.zshrc ~/.zshrc
